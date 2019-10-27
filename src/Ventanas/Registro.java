b39c2cdca71cc3076ae99b339e2d8a1fefe27c13
armand7919 <llemra79@gmail.com>
2019-10-27 15:20:54 -0600

    Septimo commit

--- src/Ventanas/Registro.java
+++ src/Ventanas/Registro.java
@@ -30,7 +30,7 @@ public class Registro extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         rButHombre = new javax.swing.JRadioButton();
-        rButMuejer = new javax.swing.JRadioButton();
+        rButMujer = new javax.swing.JRadioButton();
         jLabel3 = new javax.swing.JLabel();
         texNombre = new javax.swing.JTextField();
         textApellido = new javax.swing.JTextField();
@@ -38,44 +38,42 @@ public class Registro extends javax.swing.JFrame {
         jLabel5 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
         jLabel7 = new javax.swing.JLabel();
-        jTextField3 = new javax.swing.JTextField();
-        jTextField4 = new javax.swing.JTextField();
-        jTextField5 = new javax.swing.JTextField();
-        jTextField6 = new javax.swing.JTextField();
+        textMail = new javax.swing.JTextField();
+        textUser = new javax.swing.JTextField();
+        textPassword = new javax.swing.JTextField();
+        textPhone = new javax.swing.JTextField();
         butAceptar = new javax.swing.JButton();
         butCancelar = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jLabel1.setText("Nombre");
 
         jLabel2.setText("Apellido");
 
-        rButHombre.setText("jRadioButton1");
+        rButHombre.setText("Masculino");
+        rButHombre.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                rButHombreActionPerformed(evt);
+            }
+        });
 
-        rButMuejer.setText("jRadioButton2");
+        rButMujer.setText("Femenino");
+        rButMujer.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                rButMujerActionPerformed(evt);
+            }
+        });
 
-        jLabel3.setText("jLabel3");
+        jLabel3.setText("Sexo");
 
-        texNombre.setText("jTextField1");
+        jLabel4.setText("Correo electrónico");
 
-        textApellido.setText("jTextField2");
+        jLabel5.setText("Usuario");
 
-        jLabel4.setText("jLabel4");
+        jLabel6.setText("Contraseña");
 
-        jLabel5.setText("jLabel5");
-
-        jLabel6.setText("jLabel6");
-
-        jLabel7.setText("jLabel7");
-
-        jTextField3.setText("jTextField3");
-
-        jTextField4.setText("jTextField4");
-
-        jTextField5.setText("jTextField5");
-
-        jTextField6.setText("jTextField6");
+        jLabel7.setText("Número telefónico");
 
         butAceptar.setText("Aceptar");
 
@@ -86,44 +84,41 @@ public class Registro extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(43, 43, 43)
-                .addComponent(jLabel3)
-                .addGap(29, 29, 29)
-                .addComponent(rButHombre)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(rButMuejer)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(layout.createSequentialGroup()
                 .addGap(31, 31, 31)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel4)
                             .addComponent(jLabel7)
                             .addComponent(jLabel6)
                             .addComponent(jLabel5)
                             .addGroup(layout.createSequentialGroup()
-                                .addGap(75, 75, 75)
+                                .addComponent(jLabel4)
+                                .addGap(8, 8, 8)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addComponent(jTextField3)
-                                    .addComponent(jTextField4)
-                                    .addComponent(jTextField5)
-                                    .addComponent(jTextField6, javax.swing.GroupLayout.PREFERRED_SIZE, 195, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                        .addContainerGap(66, Short.MAX_VALUE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel1)
-                            .addComponent(jLabel2))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(texNombre, javax.swing.GroupLayout.DEFAULT_SIZE, 172, Short.MAX_VALUE)
-                            .addComponent(textApellido))
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                    .addComponent(textMail)
+                                    .addComponent(textUser)
+                                    .addComponent(textPassword)
+                                    .addComponent(textPhone, javax.swing.GroupLayout.PREFERRED_SIZE, 195, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                        .addContainerGap(18, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(36, 36, 36)
                         .addComponent(butAceptar)
                         .addGap(57, 57, 57)
                         .addComponent(butCancelar)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel1)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel3))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(texNombre, javax.swing.GroupLayout.DEFAULT_SIZE, 172, Short.MAX_VALUE)
+                            .addComponent(textApellido)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addComponent(rButHombre)
+                                .addGap(18, 18, 18)
+                                .addComponent(rButMujer)))
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
         );
         layout.setVerticalGroup(
@@ -137,28 +132,28 @@ public class Registro extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
                     .addComponent(textApellido, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(39, 39, 39)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 56, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(rButHombre)
-                    .addComponent(rButMuejer)
+                    .addComponent(rButMujer)
                     .addComponent(jLabel3))
-                .addGap(29, 29, 29)
+                .addGap(50, 50, 50)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel4)
-                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(textMail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel5)
-                    .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(textUser, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel6)
-                    .addComponent(jTextField5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(textPassword, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel7)
-                    .addComponent(jTextField6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 27, Short.MAX_VALUE)
+                    .addComponent(textPhone, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(61, 61, 61)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(butAceptar)
                     .addComponent(butCancelar))
@@ -167,6 +162,22 @@ public class Registro extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void rButHombreActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rButHombreActionPerformed
+        if(rButHombre.isSelected()== true)
+            rButMujer.setEnabled(false); // TODO add your handling code here:
+        if(rButHombre.isSelected() == false){
+            rButMujer.setEnabled(true);
+        }
+    }//GEN-LAST:event_rButHombreActionPerformed
+
+    private void rButMujerActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rButMujerActionPerformed
+        if(rButMujer.isSelected()== true)
+            rButHombre.setEnabled(false); // TODO add your handling code here:
+        if(rButMujer.isSelected() == false){
+            rButHombre.setEnabled(true);
+        }
+    }//GEN-LAST:event_rButMujerActionPerformed
 
     /**
      * @param args the command line arguments
@@ -213,13 +224,13 @@ public class Registro extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
-    private javax.swing.JTextField jTextField3;
-    private javax.swing.JTextField jTextField4;
-    private javax.swing.JTextField jTextField5;
-    private javax.swing.JTextField jTextField6;
     public javax.swing.JRadioButton rButHombre;
-    public javax.swing.JRadioButton rButMuejer;
+    public javax.swing.JRadioButton rButMujer;
     public javax.swing.JTextField texNombre;
     public javax.swing.JTextField textApellido;
+    public javax.swing.JTextField textMail;
+    public javax.swing.JTextField textPassword;
+    public javax.swing.JTextField textPhone;
+    public javax.swing.JTextField textUser;
     // End of variables declaration//GEN-END:variables
 }
--- src/control/ControlRegistro.java
+++ src/control/ControlRegistro.java
@@ -9,6 +9,7 @@ import Ventanas.Registro;
 import controlBaseDatos.UsuarioDAO;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import javax.swing.JOptionPane;
 import modelo.Usuario;
 
 /**
@@ -19,7 +20,7 @@ public class ControlRegistro implements MouseListener {
 
     int verificador;
     Usuario usuarioRegistro;
-    Registro registro=new Registro();
+    private Registro registro=new Registro();
     
 
     public ControlRegistro() {
@@ -34,38 +35,46 @@ public class ControlRegistro implements MouseListener {
         this.registro.butAceptar.addMouseListener (this);
         this.registro.butCancelar.addMouseListener (this);
         //se inicializa la ventana
-        this.registro.setSize (605, 700);
+        this.registro.setSize (400, 500);
         this.registro.setVisible (true);
         System.out.println("fase 1 inicia");
         
     }
     
     public void registrar(Registro registro){
+        String genero=null;
         Usuario user=new Usuario();
-              
         UsuarioDAO usuarioDAO=new UsuarioDAO(); //variable de clase usuarioBaseDatos que aun no se ha hecho
+        if(registro.rButHombre.isEnabled())
+            genero = "Masculino";
+        if(registro.rButMujer.isEnabled())
+            genero = "Femenino";
         
+        //System.out.println(genero);Para verificar el parametro pasado
         user.setNombre(registro.texNombre.getText()); //ejemplo de como meter los datos del usuario
         user.setApellido(registro.textApellido.getText());
-        user.setSexo("Mujer");
-        user.setCorreo("Muejer@hot.com");
-        user.setUsuario("lopez78");
-        user.setContrasena("123");
+        user.setSexo(genero);
+        user.setCorreo(registro.textMail.getText());
+        user.setUsuario(registro.textUser.getText());
+        user.setContrasena(registro.textPassword.getText());
         user.setTelefono(58591623);
            
         usuarioDAO.crear(user);
         
-        System.out.println("fase 4 registra ok");
+        System.out.println("fase 4 registra ok"); //<-----verifica el nivel en que se encuentra 
          //   usuarioRegistro.setNombre(registro.texNombre.getText());
             //se pasan todos los parametros del registro
          //   usuarioBaseDatos=usuarioRegistro;//una vez lleno el usuario que regresaras lo pasas a el usuarioBaseDatos
         
     }
     
-    public int verificaCampos(){
+    public int verificaCampos(Registro registro){
         int verificador=1; //ahí que cambiar por cero, el uno sólo es para la prueba
         //usa la variable registro para que hacer las verificaciones y una vez que este todo correcto asegna 1 a la variable verificador
-        
+        if(registro.rButHombre.isEnabled() && this.registro.rButMujer.isEnabled()){
+            JOptionPane.showMessageDialog(this.registro, "Elija tipo de género");
+            verificador=0;
+        }
         System.out.println("fase 2 verifica ");
         
         return verificador;
@@ -84,8 +93,10 @@ public class ControlRegistro implements MouseListener {
 
 
     public void mouseReleased(MouseEvent e) {
+        
         if (e.getSource() == this.registro.butAceptar){
-             verificador = verificaCampos(); // llamas ala fucion para verificar los campos 
+
+             verificador = verificaCampos(this.registro); // llamas ala fucion para verificar los campos 
              if(verificador == 1){
                 System.out.println("fase 3 mouse");
                 registrar(this.registro);
